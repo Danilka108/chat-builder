2347398c8bf25a2a13abfc7c10109b3df0d30828
Danil <61464996+Danilka108@users.noreply.github.com>
2021-04-26 00:54:25 +0500

    create builder

---
+++ build.sh
@@ -0,0 +1,20 @@
+cd ../server
+npm run build
+cd ../client
+npm run build
+cd ../builder
+ 
+if [ -d "app" ]; then rm -r app; fi
+mkdir app
+
+cd ..
+
+cp -r client/dist/chat builder/app/client
+
+cp -r server/dist builder/app
+cp server/package.json builder/app
+cp server/package-lock.json builder/app
+cp builder/.prod.env builder/app/.env
+cp builder/docker-compose.yml builder/app
+cp builder/Dockerfile builder/app
+cp builder/nginx.conf builder/app
