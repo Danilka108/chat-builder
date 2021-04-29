e5491cf589f6a988854cd575ede203d200da4eea
Danil <61464996+Danilka108@users.noreply.github.com>
2021-04-30 01:55:19 +0500

    add https

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-cd ../server
-npm run build
-cd ../client
-npm run build
-cd ../builder
+# cd ../server
+# npm run build
+# cd ../client
+# npm run build
+# cd ../builder
  
 if [ -d "app" ]; then rm -r app; fi
 mkdir app
@@ -18,3 +18,7 @@ cp builder/.prod.env builder/app/.env
 cp builder/docker-compose.yml builder/app
 cp builder/Dockerfile builder/app
 cp builder/nginx.conf builder/app
+
+cd builder/app
+mkdir dhparam
+sudo openssl dhparam -out dhparam/dhparam-2048.pem 2048
